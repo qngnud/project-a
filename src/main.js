d6f7f7d22b196836beeb3d7a8b1f6884fe9bd928
No Face <qngnud@gmail.com>
2019-12-16 13:31:58 +0700

    add vuetify

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 
 new Vue({
+    vuetify,
     render: h => h(App)
 }).$mount('#app');
