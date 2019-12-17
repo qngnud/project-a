dc473d7e5169d243d07ec6bd027d6671fe46f0ed
Hoang-Khai <57392749+Hoang-Khai@users.noreply.github.com>
2019-12-17 13:19:25 +0700

    Merge branch 'master' of https://github.com/qngnud/project-a
    # Conflicts:
    #	backend/app.js
    #	package-lock.json
    #	package.json

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 let express = require('express');
 let admin = require('firebase-admin');
-let b = require('./model/Brand');
 
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
@@ -12,29 +11,6 @@ fs.settings(settings);
 
 let app = express();
 app.get('/',async function (req, res) {
-    // let arr=[];
-    // let brandList = fs.collection('brand');
-    // let feedbackList = await brandList.get()
-    //     .then(snapshot => {
-    //         snapshot.forEach(doc => {
-    //             console.log(doc.id, '=>', doc.data());
-    //             arr.push(doc.data());
-    //         });
-    //     })
-    //     .catch(err => {
-    //         console.log("gi do");
-    //     });
-    // await res.json(arr);
-    // res.end();
-
-    // let arr=[];
-    // for(let i=0; i<1000; i++) {
-    //     let obj = b.Brand(i,'name'+i, 'image'+i, 'sound'+i, 'bg'+i,'detail'+i);
-    //     // arr.push(b);
-    //     let addDoc = fs.collection('brand').add(obj).then(ref => {
-    //         console.log('id = ', ref.id);
-    //     });
-    // }
     let arr = [];
     let pageNumber = req.query.pageno;
     if (pageNumber==null) pageNumber=0;
